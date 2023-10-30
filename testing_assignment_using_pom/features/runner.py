2b156c08a5a486177c9868baa71463f9525e4d76
Sudhanshu Pandey <65806439+sudhanshupandey123@users.noreply.github.com>
2023-10-30 10:21:04 +0530

    Update runner.py

--- testing_assignment_using_pom/features/runner.py
+++ testing_assignment_using_pom/features/runner.py
@@ -1,4 +1,5 @@
 import subprocess
 if __name__ == '__main__':
 #command line args along with error capture on failure with check true
-      s = subprocess.run('behave --no-capture',shell=True, check=True)
+      # s = subprocess.run('behave --no-capture',shell=True, check=True) 
+      s = subprocess.run('behave -f allure_behave.formatter:AllureFormatter -o reports/features',shell=True, check=True)
